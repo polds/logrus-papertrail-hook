6341f844c9b3e6d5b21e8fe57a7cd81babbe4bf7
Peter Olds <polds@kyanicorp.com>
2015-10-15 21:09:53 -0600

    Real Papertrail test
    Signed-off-by: Peter Olds <polds@kyanicorp.com>

--- papertrail_test.go
+++ papertrail_test.go
@@ -2,9 +2,12 @@ package logrus_papertrail
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/polds/logrus-papertrail-hook"
 	"github.com/stvp/go-udp-testing"
 )
 
@@ -12,7 +15,7 @@ func TestWritingToUDP(t *testing.T) {
 	port := 16661
 	udp.SetAddr(fmt.Sprintf(":%d", port))
 
-	hook, err := NewPapertrailHook(&Hook{
+	hook, err := logrus_papertrail.NewPapertrailHook(&logrus_papertrail.Hook{
 		Host:     "localhost",
 		Port:     port,
 		Hostname: "test.local",
@@ -28,4 +31,23 @@ func TestWritingToUDP(t *testing.T) {
 	udp.ShouldReceive(t, "foo", func() {
 		log.Info("foo")
 	})
+}
+
+func TestReal(t *testing.T) {
+	port, _ := strconv.Atoi(os.Getenv("PAPERTRAIL_PORT"))
+
+	hook, err := logrus_papertrail.NewPapertrailHook(&logrus_papertrail.Hook{
+		Host:     os.Getenv("PAPERTRAIL_HOST"),
+		Port:     port,
+		Hostname: "appserver",
+		Appname:  "myapp",
+	})
+	if err != nil {
+		t.Errorf("Unable to connect to Papertrail")
+	}
+
+	log := logrus.New()
+	log.Hooks.Add(hook)
+
+	log.Infoln("testing")
 }
